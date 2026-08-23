ffc7f0ef1488f3a65daa8efee48ad18c5c4551a8
vipulpink1119-design <vipulpink1119@gmail.com>
2026-08-23 21:25:42 +0530

    sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,21 @@
-self.addEventListener("install", event => {
-    self.skipWaiting();
+const CACHE_NAME = 'gunotsav-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener("activate", event => {
-    event.waitUntil(clients.claim());
-});
-
-self.addEventListener("fetch", event => {
-    event.respondWith(fetch(event.request));
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
